def0cf907d9c803be3791a3037a125831a709417
MrDean <Paul---@users.noreply.github.com>
2019-07-16 17:56:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 'use strict'
-
-let ingredients = 'i=chocolate';
+const apiKey = `7e581f01be536fa35f7d36e2746fa8ed`;
+let ingredients = 'chocolate';
 
 //event listeners
 $('.search-btn').on('click submit',  function (e) {
   e.preventDefault();
-  e.which===13? search():null;
-  search();
+  search($('#search-ingredients').val() );
 });
 
-async function search(){
-  let ingString = $('#search-ingredients').val();
-  let formattedIngs = formatIngredients(ingString);
+async function search(query){
+  let formattedIngs = formatIngredients(query);
   let recipeArr = await returnRecipeArray(formattedIngs);
   await displayRecipes(recipeArr);
   $('#search-ingredients').val('');
 }
 
 //end of event listeners
-
+//***********************************************************
 async function returnRecipeArray(ingredients) {
-  const corsWorkAround = `https://api.allorigins.win/get?url=`;
-  let URL = `${corsWorkAround}${encodeURIComponent('http://www.recipepuppy.com/api/?')}${ingredients}`;
-  let resultsArr = await fetch(URL).then(res => res.json()).then(res => JSON.parse(res.contents)).then(res => res.results);
+  let URL = `https://www.food2fork.com/api/search?key=${apiKey}&q=${ingredients}`;
+  let resultsArr = await fetch(URL).then(res => res.json()).then(res => res);
   return resultsArr;
 }
 
 async function displayRecipes(recipeArr) {
   $('.recipe-list').html('');
-  recipeArr.forEach(el => {
-    $(`<li>${el.title} <br> <a href="${el.href}" target="_blank" ><img src='${el.thumbnail}' alt=${el.title} /> </a> <br> ${el.ingredients}
+  recipeArr.recipes.forEach(el => {
+    $(`<li class="recipe-li"> <a href="https://www.food2fork.com/api/get?key=${apiKey}&rId=35382" target="_blank" ><img class="thumbnail-img" src='${el.image_url}' alt=${el.title} /> </a> <div class='recipe-title-div'> <h4 class="recipe-title">${el.title}</h4></div>
     </li>`).appendTo('.recipe-list');
   })
 }
@@ -41,7 +38,7 @@ function displayShoppingList() {
 function formatIngredients(ingredients) {
   //  return commas separated string of ingredients
   let arr = ingredients.split(' ').join(',')
-  return `i=${arr}`;
+  return `${arr}`;
 }
 
 async function loadPage(ingredients) {
