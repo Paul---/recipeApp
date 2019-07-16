a52396fad7ebf0852c9e3734b6b0907fcbd6072d
MrDean <Paul---@users.noreply.github.com>
2019-07-16 11:31:08 -0600

    make initial recipes show in side list

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //make sure to use jQuery
 'use strict'
 
-const puppyURL = `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/`
 
-fetch(puppyURL).then( res=>res.json() ).then(res=>console.log(res))
+const fetchURL =(ingredients)=>{
+  const corsWorkAround = `https://api.allorigins.win/get?url=`
+  let puppyURL = `${corsWorkAround}${encodeURIComponent('http://www.recipepuppy.com/api/?')}`
+  fetch(puppyURL).then(res => res.json()).then(res => JSON.parse(res.contents)).then(res => res.results.forEach(el => {
+    $(`<li>${el.title} <br> <a href="${el.href}" target="_blank" ><img src='${el.thumbnail}' alt=${el.title} /> </a> <br> ${el.ingredients}
+    </li>`).appendTo('.recipe-list')
+  }) );
+  
+}
+
+fetchURL();
