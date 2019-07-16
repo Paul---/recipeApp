b63d3e1a44373dba8c9250901ddeecbae6a5e539
MrDean <Paul---@users.noreply.github.com>
2019-07-16 10:05:54 -0600

    add fetch url and basic html elements

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 //make sure to use jQuery
 'use strict'
 
+const puppyURL = `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/`
+
+fetch(puppyURL).then( res=>res.json() ).then(res=>console.log(res))
