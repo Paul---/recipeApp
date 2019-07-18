8653f46d1ff1908a7728706251fc8a1b71cced5a
MrDean <Paul---@users.noreply.github.com>
2019-07-18 13:53:13 -0600

    add button style

--- index.js
+++ index.js
@@ -6,13 +6,14 @@
 // 340c0e8204c153aec46ae2103ad8e6eb
 // 930bc5b5a9b1fb99dcb2a2e011f64900
 // 86bdf91aef7ebad9e0ff67d0b34be435
-// e9bbd0a67061b4725aab9461813323c3
+
 //used up
+// e9bbd0a67061b4725aab9461813323c3
 // da5cea6d324729686c62d2d31d91b0be
 // cdcaf77900cc416836fc5f4cdba35d47
 // 3a2fbfa047a5b46ac2a4491aa18105c8
 // 5137c8f4084b3e19a8803e2baf2f7604
-const apiKey = `da5cea6d324729686c62d2d31d91b0be`;
+const apiKey = `e9bbd0a67061b4725aab9461813323c3`;
 let ingredients = '';
 
 //event listeners ********************************************************
@@ -39,14 +40,14 @@ async function returnRecipeArray(ingredients) {
 async function displayRecipes(recipeArr) {
   $('.recipe-list').html('');
   recipeArr.recipes.forEach(el => {
-    $(`<a href='#top'><li value="${el.recipe_id}" class="recipe-li"><img class="thumbnail-img" src='${el.image_url}' alt=${el.title} /><div class='recipe-title-div'><h4 class="recipe-title">${el.title}</h4></div>
-    </li></a>`).appendTo('.recipe-list')
+    $(`<li value="${el.recipe_id}" class="recipe-li"><img class="thumbnail-img" src='${el.image_url}' alt=${el.title} /><div class='recipe-title-div'><h4 class="recipe-title">${el.title}</h4></div>
+    </li>`).appendTo('.recipe-list')
   });
   //add event listener to each li
   $('li').on('click', function (e) {
     e.preventDefault();
     displaySelectedRecipe($(e.target).closest('li').val());
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 
   });
 }
@@ -56,11 +57,10 @@ async function search(query) {
   let recipeArr = await returnRecipeArray(formattedIngs);
   await displayRecipes(recipeArr);
   $('#search-ingredients').val('');
+  $('.selected-recipe-sec').html('');
 }
 
 async function displaySelectedRecipe(recipeId) {
-  //remove list of recipes
-  //$('.recipe-list').html('');
   //fetch individual recipe
   let chosenRecipe = await fetch(`https://www.food2fork.com/api/get?key=${apiKey}&rId=${recipeId}`).then(res => res.json());
   //display large img with description and ingredients for printing
